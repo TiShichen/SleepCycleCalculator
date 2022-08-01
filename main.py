1b86b602dc8ae2929921ccf20e6270ca0e8e289b
TiShichen <acemichael2015@gmail.com>
2022-08-01 18:30:28 +0800

    Added build and dist folder into .gitignore

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter as tk
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# .gitignore added build and dist folder
 
 def calculateCycle():
 
